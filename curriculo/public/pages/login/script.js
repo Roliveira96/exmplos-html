4ff33dc77709f5ae8d227976fd865e3497056fda
Ricardo de Oliveira <ricardo.oliveira@dgtallab.com.br>
2026-03-05 09:49:47 -0300

    feat: adição de novos testes

--- curriculo/public/pages/login/script.js
+++
@@ -1,7 +0,0 @@
-const urlParams = new URLSearchParams(window.location.search);
-const error = urlParams.get('error');
-if (error) {
-    const el = document.getElementById('error-msg');
-    el.textContent = error;
-    el.classList.remove('hidden');
-}
